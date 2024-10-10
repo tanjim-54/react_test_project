49d76ca3597da49182fe12c2328453db9400d1df
tanjim-54 <tanjimahmed1161@gmail.com>
2024-10-10 13:24:41 +0600

    "test"

--- src/components/FeedPage.js
+++ src/components/FeedPage.js
@@ -19,8 +19,7 @@ const FeedPage = () => {
 					<div className="_layout_inner_wrap">
 						<div className="row">
 							
-							
-						{/* <!-- Left Sidebar --> */}
+						{/* <!-- Left Sidebar tanjim --> */}
 						<div className="col-xl-3 col-lg-3 col-md-12 col-sm-12">
 							<div className="_layout_left_sidebar_wrap">
 								<div className="_layout_left_sidebar_inner">
